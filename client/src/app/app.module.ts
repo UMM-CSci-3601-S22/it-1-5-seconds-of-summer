5059de35873fa886995c8bb00707ff6b4317f07f
Dongting Cai <cai00151@morris.umn.edu>
2022-03-03 16:34:11 -0600

    Start Add Product/Shopping Management System/Page
    < Related with Issue/Epic #40 >
    Finished Issue #54, which added the product.ts with the variables will be used;
    Start Issue #42, but only generate the Product-List component without doing anything else.
    
    Co-authored-by: Yubo Mao <mao00071@morris.umn.edu>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { AddUserComponent } from './users/add-user.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
@@ -60,6 +61,7 @@ const MATERIAL_MODULES: any[] = [
     UserCardComponent,
     UserProfileComponent,
     AddUserComponent,
+    ProductListComponent,
   ],
   imports: [
     BrowserModule,
