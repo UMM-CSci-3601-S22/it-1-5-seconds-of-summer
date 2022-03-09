18ec4176655f8dc7306cc46cd5fb3ce876c562c5
Dongting Cai <cai00151@morris.umn.edu>
2022-03-09 10:14:40 -0600

    Product Client | Test Coverage Continuing: add-product
    < Related with Issue/Epic #40 >
    Issue #44:
    Continuing add-product test coverage.

--- client/src/app/product/add-product/add-product.component.spec.ts
+++ client/src/app/product/add-product/add-product.component.spec.ts
@@ -1,25 +1,109 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule, FormGroup, AbstractControl } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockProductService } from 'src/testing/product.service.mock';
 import { AddProductComponent } from './add-product.component';
+import { ProductService } from '../product.service';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 describe('AddProductComponent', () => {
-  let component: AddProductComponent;
+  let addProductComponent: AddProductComponent;
+  let addProductForm: FormGroup;
   let fixture: ComponentFixture<AddProductComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddProductComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        RouterTestingModule
+      ],
+      declarations: [AddProductComponent],
+      providers: [{ provide: ProductService, useValue: new MockProductService() }]
+    }).compileComponents().catch(error => {
+      expect(error).toBeNull();
+    });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AddProductComponent);
-    component = fixture.componentInstance;
+    addProductComponent = fixture.componentInstance;
+    addProductComponent.ngOnInit();
     fixture.detectChanges();
+    addProductForm = addProductComponent.addProductCtrl;
+    expect(addProductForm).toBeDefined();
+    expect(addProductForm.controls).toBeDefined();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // Not terribly important; if the component doesn't create
+  // successfully that will probably blow up a lot of things.
+  // Including it, though, does give us confidence that our
+  // our component definitions don't have errors that would
+  // prevent them from being successfully constructed.
+  it('should create the component and form', () => {
+    expect(addProductComponent).toBeTruthy();
+    expect(addProductForm).toBeTruthy();
+  });
+
+  // Confirms that an initial, empty form is *not* valid, so
+  // people can't submit an empty form.
+  it('form should be invalid when empty', () => {
+    expect(addProductForm.valid).toBeFalsy();
+  });
+
+  describe('The name field', () => {
+    let nameControl: AbstractControl;
+
+    beforeEach(() => {
+      nameControl = addProductComponent.addProductCtrl.controls.name;
+    });
+
+    it('should not allow empty names', () => {
+      nameControl.setValue('');
+      expect(nameControl.valid).toBeFalsy();
+    });
+
+    it('should be fine with "White Chocolate"', () => {
+      nameControl.setValue('White Chocolate');
+      expect(nameControl.valid).toBeTruthy();
+    });
+
+    it('should fail on single character names', () => {
+      nameControl.setValue('x');
+      expect(nameControl.valid).toBeFalsy();
+      expect(nameControl.hasError('minlength')).toBeTruthy();
+    });
+
+    it('should fail on really long names', () => {
+      nameControl.setValue('x'.repeat(100));
+      expect(nameControl.valid).toBeFalsy();
+      expect(nameControl.hasError('maxlength')).toBeTruthy();
+    });
+
+    it('should allow digits in the name', () => {
+      nameControl.setValue('Bad2Th3B0ne');
+      expect(nameControl.valid).toBeTruthy();
+    });
+
+    it('should fine if we provide an "existing" name', () => {
+
+      nameControl.setValue('abc123');
+      expect(nameControl.valid).toBeTruthy();
+
+      nameControl.setValue('123abc');
+      expect(nameControl.valid).toBeTruthy();
+    });
   });
 });
